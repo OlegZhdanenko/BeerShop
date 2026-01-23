6bf1abeb66488831c8571f65bfda8283c38384ba
OlegZhdanenko <125878669+OlegZhdanenko@users.noreply.github.com>
2026-01-23 11:04:47 +0200

    sum

--- frontend/src/lib/ton.ts
+++ frontend/src/lib/ton.ts
@@ -1,5 +1,6 @@
 import { TonConnect } from "@tonconnect/sdk";
 
 export const tonConnect = new TonConnect({
-  manifestUrl: "https://beer-app.vercel.app/tonconnect-manifest.json",
+  manifestUrl:
+    "https://beer-app.vercel.app/frontend/public/tonconnect-manifest.json",
 });
